04b51adac53cf9ae1f434fd8fbb8cfec52170f41
Neaz-mq <neazmorshed666@gmail.com>
2025-08-23 16:02:17 +0600

    navbar and images added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from './components/Navbar';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
  return (
 	<main>
-	 <div>
-        <h1 className='text-7xl text-amber-500'>sfsfsf</h1>
-     </div>
+	 <Navbar />
 	</main>
  )
 }
